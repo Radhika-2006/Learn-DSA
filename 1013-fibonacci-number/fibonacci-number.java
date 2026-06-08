d0e16a7b668bbb950be7dbd8c2e551be484eecef
Radhika Agarwal <agarwalradhika432@gmail.com>
2026-06-08 13:44:56 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (50.69%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,7 +1,11 @@
 class Solution {
     public int fib(int n){
-        if(n==0) return 0;
-        if(n==1) return 1;
-        return fib(n-1)+fib(n-2);
+        int dp[]=new int[n+1];
+        return f(n,dp);
+    }
+    int f(int n,int dp[]){
+        if(n<=1) return n;
+        if(dp[n]!=0)return dp[n];
+        return dp[n]=f(n-1,dp)+f(n-2,dp);
     }
 }
