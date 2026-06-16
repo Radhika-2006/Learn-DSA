586361e8753330404a8ac45429808399ba6e9c32
Radhika Agarwal <agarwalradhika432@gmail.com>
2026-06-16 10:07:49 +0530

    Time: 10 ms (52.36%) | Memory: 46.9 MB (55.32%) - LeetSync

--- 838-design-linked-list/design-linked-list.java
+++ 838-design-linked-list/design-linked-list.java
@@ -1,111 +1,89 @@
 class MyLinkedList {
+    Node head; //null
+    int size; //0
 
-    class Node {
-        int val;
+    class Node{
+        int data;
         Node next;
-
-        Node(int val) {
-            this.val = val;
-            this.next = null;
+        Node(int d){
+            data=d;
         }
     }
-
-    Node head;
-    int size;
-
     public MyLinkedList() {
-        head = null;
-        size = 0;
+        head=null;
+        size=0;
     }
-
+    
     public int get(int index) {
-        if (index < 0 || index >= size) {
-            return -1;
-        }
-
-        Node temp = head;
-
-        for (int i = 0; i < index; i++) {
-            temp = temp.next;
-        }
-
-        return temp.val;
+        if(index<0 || index>=size) return -1;
+        Node temp=head;
+        for(int i=0; i<index; i++)
+        temp=temp.next;
+        return temp.data;
     }
-
+    
     public void addAtHead(int val) {
-        Node newNode = new Node(val);
-
-        newNode.next = head;
-        head = newNode;
-
+        Node n=new Node(val);
+        n.next=head;
+        head=n;
         size++;
     }
-
+    
     public void addAtTail(int val) {
-        Node newNode = new Node(val);
-
-        if (head == null) {
-            head = newNode;
-        } else {
-            Node temp = head;
-
-            while (temp.next != null) {
-                temp = temp.next;
-            }
-
-            temp.next = newNode;
-        }
-
+        Node n=new Node(val);
         size++;
-    }
-
-    public void addAtIndex(int index, int val) {
-        if (index < 0 || index > size) {
+        if(head==null){
+            head=n;
             return;
         }
-
-        if (index == 0) {
+        Node temp=head;
+        while(temp.next!=null)
+        temp=temp.next;
+        temp.next=n;
+    }
+    
+    public void addAtIndex(int index, int val) {
+        if(index<0 || index>size)
+        return;
+        if (index==size){
+            addAtTail(val);
+            return;
+        }
+        if(index==0){
             addAtHead(val);
             return;
         }
 
-        if (index == size) {
-            addAtTail(val);
-            return;
-        }
-
-        Node newNode = new Node(val);
-        Node temp = head;
-
-        for (int i = 0; i < index - 1; i++) {
-            temp = temp.next;
-        }
-
-        newNode.next = temp.next;
-        temp.next = newNode;
-
+        Node n=new Node(val);
+        Node temp=head;
+        for(int i=0; i<index-1; i++)
+        temp=temp.next;
+        n.next=temp.next;
+        temp.next=n;
         size++;
     }
-
+    
     public void deleteAtIndex(int index) {
-        if (index < 0 || index >= size) {
-            return;
-        }
-
-        if (index == 0) {
-            head = head.next;
-            size--;
-            return;
-        }
-
-        Node temp = head;
-
-        for (int i = 0; i < index - 1; i++) {
-            temp = temp.next;
-        }
-
-        temp.next = temp.next.next;
-
+        if(index<0 || index>=size) //invalid case
+        return;
         size--;
+        if(index==0){
+            head=head.next;
+            return;
+        }
+        Node temp=head;
+        for(int i=0; i<index-1; i++)
+        temp=temp.next;
+        temp.next=temp.next.next;
     }
 }
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.get(index);
+ * obj.addAtHead(val);
+ * obj.addAtTail(val);
+ * obj.addAtIndex(index,val);
+ * obj.deleteAtIndex(index);
+ */
