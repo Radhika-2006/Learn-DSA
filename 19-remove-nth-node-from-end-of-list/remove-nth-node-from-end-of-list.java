a941c8f43f3d5ddc4754e1ba54ba53484710f18d
Radhika Agarwal <agarwalradhika432@gmail.com>
2026-06-18 13:43:47 +0530

    Time: 1 ms (2.64%) | Memory: 43.5 MB (39.01%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -8,20 +8,40 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode removeNthFromEnd(ListNode head, int n) {
+//         ListNode dummy =new ListNode(0);
+//         dummy.next=head;
+//         ListNode fast = dummy;
+//         ListNode slow = dummy;
+//         for(int i=0;i<=n;i++){
+//             fast = fast.next;
+//         }
+//         while(fast != null){
+//             fast = fast.next;
+//             slow = slow.next;
+//         }
+//         slow.next = slow.next.next;
+//         return dummy.next;
+//     }
+// }
+
+//  by arraylist
+
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode dummy =new ListNode(0);
-        dummy.next=head;
-        ListNode fast = dummy;
-        ListNode slow = dummy;
-        for(int i=0;i<=n;i++){
-            fast = fast.next;
+        ArrayList<ListNode> list=new ArrayList<>();
+        ListNode current = head;
+        while(current !=null){
+            list.add(current);
+            current=current.next;
         }
-        while(fast != null){
-            fast = fast.next;
-            slow = slow.next;
+        int size=list.size();
+        int removeIndex=size-n;
+        if(removeIndex==0){
+            return head.next;
         }
-        slow.next = slow.next.next;
-        return dummy.next;
+        ListNode previous=list.get(removeIndex - 1);
+     previous.next=previous.next.next;return head;
     }
 }
