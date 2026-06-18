ca3762586c53fd5827c96f594b07bee25f334dab
Radhika Agarwal <agarwalradhika432@gmail.com>
2026-06-18 13:22:20 +0530

    Time: 6 ms (9.89%) | Memory: 46.6 MB (48.85%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -1,27 +1,43 @@
+// public class Solution { 
+//     public ListNode detectCycle(ListNode head) {
+
+//         ListNode slow = head;
+//         ListNode fast = head;
+
+//         while (fast != null && fast.next != null) {
+
+//             slow = slow.next;
+//             fast = fast.next.next;
+
+//             if (slow == fast) {
+
+//                 ListNode start = head;
+
+//                 while (start != slow) {
+//                     start = start.next;
+//                     slow = slow.next;
+//                 }
+
+//                 return start;
+//             }
+//         }
+
+//         return null;
+//     }
+// }
+
+// by hashset
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-
-        ListNode slow = head;
-        ListNode fast = head;
-
-        while (fast != null && fast.next != null) {
-
-            slow = slow.next;
-            fast = fast.next.next;
-
-            if (slow == fast) {
-
-                ListNode start = head;
-
-                while (start != slow) {
-                    start = start.next;
-                    slow = slow.next;
-                }
-
-                return start;
+        HashSet<ListNode> set = new HashSet<>();
+        ListNode temp = head;
+        while (temp != null) {
+            if (set.contains(temp)) {
+                return temp;
             }
+            set.add(temp);
+            temp = temp.next;
         }
-
         return null;
     }
 }
